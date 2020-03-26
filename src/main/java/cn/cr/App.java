a711d7e9fd8c1bbf3fe09c3a3d69d3b09c743e9b
carons-cr <2738794789@qq.com>
2020-03-26 21:24:10 +0800

    mybatis auto create dao and dataobject.

--- src/main/java/cn/cr/App.java
+++ src/main/java/cn/cr/App.java
@@ -1,7 +1,12 @@
 package cn.cr;
 
+import cn.cr.dao.UserDOMapper;
+import cn.cr.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"cn.cr"})
 @RestController
+@MapperScan("cn.cr.dao")
 public class App 
 {
-    public static void main( String[] args ) {
-        SpringApplication.run(App.class, args);
-    }
+
+
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String hello(){
-        return "hello world!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null) {
+            return "hello world!";
+        }else {
+            return userDO.getName();
+        }
+    }
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
