b41cc7ef21ea9b40ae532d366b0414e79e6c8bbd
carons-cr <2738794789@qq.com>
2020-03-28 11:21:35 +0800

    complete /getotp back end code.

--- src/main/java/cn/cr/controller/UserController.java
+++ src/main/java/cn/cr/controller/UserController.java
@@ -13,12 +13,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @Controller("user")
 @RequestMapping("/user")
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telephone") String telephone){
+        //按照一定规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(9000);
+        randomInt += 1000;
+        String otpCode = String.valueOf(randomInt);
+        //将otp验证码同对应用户的手机号关联
+        httpServletRequest.getSession().setAttribute(telephone, otpCode);
+        //将otp验证码通过短信通道发送给用户,暂时省略
+        System.out.println("telephone = "+telephone+" & otpCode = "+otpCode);
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
